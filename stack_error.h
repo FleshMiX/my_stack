311ca0e71b63a646a63645f90dca44eed9292ad2
FleshMiX <31885817+FleshMiX@users.noreply.github.com>
2019-11-17 03:40:12 +0300

    Create stack_error.h

---
+++ stack_error.h
@@ -0,0 +1,18 @@
+//
+// Created by Fleshmix on 02.11.2019.
+//
+
+#ifndef STACK_STACK_ERROR_H
+#define STACK_STACK_ERROR_H
+
+#define NULL_DATA_POINTER 101
+#define ZERO_CAPACITY 102
+#define BAD_REALLOC 103
+#define NEGATIVE_SIZE 104
+#define BAD_DATA_HASH 105
+#define DEAD_DATA_CANARY 106
+#define DEAD_STACK_CANARY 107
+#define STACK_ZERO_POINTER 108
+#define BAD_STACK_HASH 109
+
+#endif //STACK_STACK_ERROR_H
